f7c03042c042e115230f2f7c7780af057a039f39
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-28 22:53:18 +0530

    To work on meeting room

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Loader from "@/components/Loader";
 import MeetingRoom from "@/components/MeetingRoom";
 import MeetingSetup from "@/components/MeetingSetup";
 import { useGetCallById } from "@/hooks/useGetCallById";
@@ -12,11 +13,17 @@ const Meeting = ({ params: { id } }: { params: { id: string } }) => {
   const [isSetupComplete, setIsSetupComplete] = useState(false);
   const { call, isCallLoading } = useGetCallById(id);
 
+  if (!isLoaded || isCallLoading) return <Loader />;
+
   return (
     <main className="h-screen w-full">
-      <StreamCall>
+      <StreamCall call={call}>
         <StreamTheme>
-          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
         </StreamTheme>
       </StreamCall>
     </main>
