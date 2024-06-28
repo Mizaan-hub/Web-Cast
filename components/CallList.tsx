7433adef49f68e6e551b5c6be58d32e58d5a9bb6
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-29 03:17:38 +0530

    used custom hook usegetCalls in Call List

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import Upcoming from '../app/(root)/(home)/upcoming/page';
+import { useGetCalls } from "@/hooks/useGetCalls";
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
+
+  const {endedCalls, upcomingCalls, recordings, isLoading} = useGetCalls()
+
   return <div>CallList</div>;
 };
 
