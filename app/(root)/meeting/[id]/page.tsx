3b3b52763858d6893a36a52cbade7dd1204a75d5
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-28 22:11:32 +0530

    Succesfully added custom hook in meeting page

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -2,23 +2,21 @@
 
 import MeetingRoom from "@/components/MeetingRoom";
 import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
 import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import { useState } from "react";
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+const Meeting = ({ params: { id } }: { params: { id: string } }) => {
   const { user, isLoaded } = useUser();
-  const [isSetupComplete, setIsSetupComplete] = useState(false)
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(id);
 
   return (
     <main className="h-screen w-full">
-      <StreamCall >
+      <StreamCall>
         <StreamTheme>
-          {!isSetupComplete? (
-            <MeetingSetup/>
-          ):(
-            <MeetingRoom/>
-          )}
+          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
         </StreamTheme>
       </StreamCall>
     </main>
