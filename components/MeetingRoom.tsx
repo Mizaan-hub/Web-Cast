3a5b2091c5f5b0c30e6c736ce00e7d84b24bf69c
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-29 01:39:19 +0530

    fixed meeting room call participants layout

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,47 +1,48 @@
-import { cn } from "@/lib/utils";
+"use client";
+import { useState } from "react";
 import {
   CallControls,
-  CallingState,
   CallParticipantsList,
   CallStatsButton,
+  CallingState,
   PaginatedGridLayout,
   SpeakerLayout,
   useCallStateHooks,
 } from "@stream-io/video-react-sdk";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Users, LayoutList } from "lucide-react";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import React, { useState } from "react";
-import { LayoutList, Users } from "lucide-react";
-import { useSearchParams } from "next/navigation";
-import EndCallButton from "./EndCallButton";
+} from "./ui/dropdown-menu";
 import Loader from "./Loader";
+import EndCallButton from "./EndCallButton";
+import { cn } from "@/lib/utils";
 
-type CallLayoutType = "grid" | "speaker_left" | "speaker_right";
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
   const searchParams = useSearchParams();
   const isPersonalRoom = !!searchParams.get("personal");
-  const [layout, setLayout] = useState<CallLayoutType>("speaker_left");
-
+  const router = useRouter();
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
   const [showParticipants, setShowParticipants] = useState(false);
-
   const { useCallCallingState } = useCallStateHooks();
 
+  // for more detail about types of CallingState see: https://getstream.io/video/docs/react/ui-cookbook/ringing-call/#incoming-call-panel
   const callingState = useCallCallingState();
 
-  if(callingState !==CallingState.JOINED) return <Loader />
+  if (callingState !== CallingState.JOINED) return <Loader />;
 
   const CallLayout = () => {
     switch (layout) {
       case "grid":
         return <PaginatedGridLayout />;
-      case "speaker_right":
+      case "speaker-right":
         return <SpeakerLayout participantsBarPosition="left" />;
       default:
         return <SpeakerLayout participantsBarPosition="right" />;
@@ -62,20 +63,20 @@ const MeetingRoom = () => {
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
+      {/* video layout and call controls */}
+      <div className="fixed bottom-3 flex w-full items-center justify-center gap-5">
+        <CallControls onLeave={() => router.push(`/`)} />
 
-      <div className="fixed bottom-3 flex w-full items-center justify-center gap-5 flex-wrap">
-        <CallControls />
         <DropdownMenu>
           <div className="flex items-center">
             <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
-              <LayoutList className="text-white" size={20} />
+              <LayoutList size={20} className="text-white" />
             </DropdownMenuTrigger>
           </div>
           <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
             {["Grid", "Speaker-Left", "Speaker-Right"].map((item, index) => (
               <div key={index}>
                 <DropdownMenuItem
-                  className="cursor-pointer"
                   onClick={() =>
                     setLayout(item.toLowerCase() as CallLayoutType)
                   }
@@ -88,11 +89,10 @@ const MeetingRoom = () => {
           </DropdownMenuContent>
         </DropdownMenu>
         <CallStatsButton />
-        <button
-          onClick={() => setShowParticipants((prev) => !prev)}
-          title="button"
-        >
-          <div className="curser-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+        <button onClick={() => setShowParticipants((prev) => !prev)}
+        title="end"
+          >
+          <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
             <Users size={20} className="text-white" />
           </div>
         </button>
