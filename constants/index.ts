387649ffbec7c8e345526305d8c59df08b04dd2c
Mizaan-hub <mizaanafandi@gmail.com>
2024-07-01 11:30:31 +0530

    Appropriate images

--- constants/index.ts
+++ constants/index.ts
@@ -27,9 +27,14 @@ export const sidebarLinks = [
 ];
 
 export const avatarImages = [
-  '/images/avatar-1.jpg', //"/public/images/avatar-1.jpg"
-  '/images/avatar-2.jpg', //"/public/images/avatar-2.jpg"
-  '/images/avatar-3.jpg', //"/public/images/avatar-3.jpg"
-  '/images/avatar-4.png', //"/public/images/avatar-4.png"
-  '/images/avatar-5.jpg', //"/public/images/avatar-5.jpg"
+  // '/images/avatar-1.jpg', //"/public/images/avatar-1.jpg"
+  // '/images/avatar-2.jpg', //"/public/images/avatar-2.jpg"
+  // '/images/avatar-3.jpg', //"/public/images/avatar-3.jpg"
+  // '/images/avatar-4.png', //"/public/images/avatar-4.png"
+  // '/images/avatar-5.jpg', //"/public/images/avatar-5.jpg"
+  '/images/profile-1.jpeg', //"/public/images/profile-1.jpeg"
+  '/images/profile-2.jpeg', //"/public/images/profile-2.jpeg"
+  '/images/profile-3.jpeg', //"/public/images/profile-3.jpeg"
+  '/images/profile-4.png', //"/public/images/profile-4.png"
+  '/images/profile-5.jpp.jpg', //"/public/images/profile-5.jpp.jpg"
 ];
