9c5615acb1b97ff0cfe2780751e15d6cf939c0fc
Mizaan-hub <mizaanafandi@gmail.com>
2024-07-05 14:36:16 +0530

    Updated Personal Room Link

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -29,7 +29,7 @@ const PersonalRoom = () => {
   const { user } = useUser();
   const router = useRouter();
   const meetingId = user?.id;
-  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`;
+  const meetingLink = `/meeting/${meetingId}?personal=true`;
   const client = useStreamVideoClient();
   const { call } = useGetCallById(meetingId!);
 
